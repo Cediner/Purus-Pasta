5dca3f88ddf8deb8a5f03a727e8695460d03cb4c
Cediner <cedivargardner@gmail.com>
2020-10-16 02:28:58 +0300

    fix

--- src/haven/Widget.java
+++ src/haven/Widget.java
@@ -425,6 +425,7 @@ public class Widget {
                     } else {
                         throw(new RuntimeException("Invalid division operands: " + a + " - " + b));
                     }
+                } else if(op == 'S') {
                 } else if(Character.isWhitespace(op)) {
                 } else {
                     throw(new RuntimeException("Unknown position operation: " + op));
